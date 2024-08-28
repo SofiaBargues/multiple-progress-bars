6ddd0c673918e3958f3458e9197728ed7a21216d
Sofia Bargues <barguessofia@gmail.com>
2024-08-28 22:30:13 +0100

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
 
-function App() {
+function ProgressBar({ key, charge }) {
+  if (charge > 100 && 0 > charge) {
+    // console.log("charge is not in range");x
+  }
+  return (
+    <div key={key} className="border-4 border-[#2d3f50]  h-8">
+      <div
+        style={{
+          width: charge + "%",
+        }}
+        className="bg-blue-500 h-6"
+      ></div>
+    </div>
+  );
+}
+
+export function App() {
   const [bar, setBar] = useState(0);
-  const [carga, setCarga] = useState(10);
+  const [charge, setCharge] = useState(10);
   let barras = [];
   for (let i = 0; i <= bar; i++) {
-    barras.push(
-      <div key={i} className="border-4 border-[#2d3f50]  h-8">
-        <div
-          style={{
-            width: carga + "%",
-          }}
-          className="bg-blue-500 h-6"
-        ></div>
-      </div>
-    );
+    //calculate charge for this proggres bar
+    const entero = Math.floor(charge / 100);
+    let barCharge;
+    if (i < entero) {
+      barCharge = 100;
+    } else {
+      barCharge = charge % 100;
+    }
+
+    barras.push(<ProgressBar key={i} charge={barCharge} />);
   }
 
   function handleClick() {
